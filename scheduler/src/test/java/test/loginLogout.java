097ef8331a0d9425ea7257a0e10a83202d649e40
eshete <oneshete@gmail.com>
2021-10-03 11:44:13 -0400

    chrome driver path is set to /usr/bin/google-chrome

--- scheduler/src/test/java/test/loginLogout.java
+++ scheduler/src/test/java/test/loginLogout.java
@@ -24,7 +24,7 @@ public class loginLogout {
 	@BeforeTest
 	public void beforeTest() {
 
-		System.setProperty("webdriver.chrome.driver", utility.PathList.chromeDriver);
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("start-maximized");
 		options.addArguments("disable-infobars");
